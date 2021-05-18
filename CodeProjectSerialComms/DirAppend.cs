808b2209a4cbd33d1677846f0ff3e1e21533921f
Eben Varghese Paul <810192@lulumea.com>
2021-05-18 10:37:22 +0400

    Initial commit

---
+++ CodeProjectSerialComms/DirAppend.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CodeProjectSerialComms
+{
+    class DirAppend
+    {
+        public void LogMessage(string logMessage)
+        {
+           Label t = Application.OpenForms["Form1"].Controls["lblLogger"] as Label;
+            t.Text = logMessage;
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                Log(logMessage, w);
+            }
+
+            using (StreamReader r = File.OpenText("log.txt"))
+            {
+                DumpLog(r);
+            }
+        }
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()} " + $"  : {logMessage}");
+            w.WriteLine("-------------------------------------------------------------------------------------");
+        }
+
+        public static void DumpLog(StreamReader r)
+        {
+            string line;
+            while ((line = r.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+      
+    }
+}
---
+++ CodeProjectSerialComms/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace CodeProjectSerialComms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetSerialPorts = new System.Windows.Forms.Button();
+            this.rtbIncoming = new System.Windows.Forms.RichTextBox();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.cboDataBits = new System.Windows.Forms.ComboBox();
+            this.cboStopBits = new System.Windows.Forms.ComboBox();
+            this.cboParity = new System.Windows.Forms.ComboBox();
+            this.cboHandShaking = new System.Windows.Forms.ComboBox();
+            this.lblBreakStatus = new System.Windows.Forms.Label();
+            this.lblCTSStatus = new System.Windows.Forms.Label();
+            this.lblDSRStatus = new System.Windows.Forms.Label();
+            this.lblRIStatus = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnPortState = new System.Windows.Forms.Button();
+            this.btnHello = new System.Windows.Forms.Button();
+            this.rtbOutgoing = new System.Windows.Forms.RichTextBox();
+            this.btnHyperTerm = new System.Windows.Forms.Button();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblLogger = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetSerialPorts
+            // 
+            this.btnGetSerialPorts.Location = new System.Drawing.Point(12, 27);
+            this.btnGetSerialPorts.Name = "btnGetSerialPorts";
+            this.btnGetSerialPorts.Size = new System.Drawing.Size(75, 23);
+            this.btnGetSerialPorts.TabIndex = 0;
+            this.btnGetSerialPorts.Text = "Ports";
+            this.btnGetSerialPorts.UseVisualStyleBackColor = true;
+            this.btnGetSerialPorts.Click += new System.EventHandler(this.btnGetSerialPorts_Click);
+            // 
+            // rtbIncoming
+            // 
+            this.rtbIncoming.Location = new System.Drawing.Point(12, 304);
+            this.rtbIncoming.Name = "rtbIncoming";
+            this.rtbIncoming.Size = new System.Drawing.Size(350, 96);
+            this.rtbIncoming.TabIndex = 1;
+            this.rtbIncoming.Text = "";
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(102, 29);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(121, 21);
+            this.cboPorts.TabIndex = 2;
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Location = new System.Drawing.Point(102, 56);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cboBaudRate.TabIndex = 3;
+            // 
+            // cboDataBits
+            // 
+            this.cboDataBits.FormattingEnabled = true;
+            this.cboDataBits.Location = new System.Drawing.Point(102, 83);
+            this.cboDataBits.Name = "cboDataBits";
+            this.cboDataBits.Size = new System.Drawing.Size(121, 21);
+            this.cboDataBits.TabIndex = 4;
+            // 
+            // cboStopBits
+            // 
+            this.cboStopBits.FormattingEnabled = true;
+            this.cboStopBits.Location = new System.Drawing.Point(102, 110);
+            this.cboStopBits.Name = "cboStopBits";
+            this.cboStopBits.Size = new System.Drawing.Size(121, 21);
+            this.cboStopBits.TabIndex = 5;
+            // 
+            // cboParity
+            // 
+            this.cboParity.FormattingEnabled = true;
+            this.cboParity.Location = new System.Drawing.Point(102, 137);
+            this.cboParity.Name = "cboParity";
+            this.cboParity.Size = new System.Drawing.Size(121, 21);
+            this.cboParity.TabIndex = 6;
+            // 
+            // cboHandShaking
+            // 
+            this.cboHandShaking.FormattingEnabled = true;
+            this.cboHandShaking.Location = new System.Drawing.Point(102, 164);
+            this.cboHandShaking.Name = "cboHandShaking";
+            this.cboHandShaking.Size = new System.Drawing.Size(121, 21);
+            this.cboHandShaking.TabIndex = 7;
+            // 
+            // lblBreakStatus
+            // 
+            this.lblBreakStatus.AutoSize = true;
+            this.lblBreakStatus.Location = new System.Drawing.Point(24, 191);
+            this.lblBreakStatus.Name = "lblBreakStatus";
+            this.lblBreakStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblBreakStatus.TabIndex = 8;
+            this.lblBreakStatus.Text = "Break";
+            this.lblBreakStatus.Visible = false;
+            // 
+            // lblCTSStatus
+            // 
+            this.lblCTSStatus.AutoSize = true;
+            this.lblCTSStatus.Location = new System.Drawing.Point(79, 191);
+            this.lblCTSStatus.Name = "lblCTSStatus";
+            this.lblCTSStatus.Size = new System.Drawing.Size(28, 13);
+            this.lblCTSStatus.TabIndex = 9;
+            this.lblCTSStatus.Text = "CTS";
+            this.lblCTSStatus.Visible = false;
+            // 
+            // lblDSRStatus
+            // 
+            this.lblDSRStatus.AutoSize = true;
+            this.lblDSRStatus.Location = new System.Drawing.Point(130, 191);
+            this.lblDSRStatus.Name = "lblDSRStatus";
+            this.lblDSRStatus.Size = new System.Drawing.Size(30, 13);
+            this.lblDSRStatus.TabIndex = 10;
+            this.lblDSRStatus.Text = "DSR";
+            this.lblDSRStatus.Visible = false;
+            // 
+            // lblRIStatus
+            // 
+            this.lblRIStatus.AutoSize = true;
+            this.lblRIStatus.Location = new System.Drawing.Point(188, 191);
+            this.lblRIStatus.Name = "lblRIStatus";
+            this.lblRIStatus.Size = new System.Drawing.Size(18, 13);
+            this.lblRIStatus.TabIndex = 11;
+            this.lblRIStatus.Text = "RI";
+            this.lblRIStatus.Visible = false;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(12, 135);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 12;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Visible = false;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnPortState
+            // 
+            this.btnPortState.Location = new System.Drawing.Point(12, 56);
+            this.btnPortState.Name = "btnPortState";
+            this.btnPortState.Size = new System.Drawing.Size(75, 23);
+            this.btnPortState.TabIndex = 13;
+            this.btnPortState.Text = "Closed";
+            this.btnPortState.UseVisualStyleBackColor = true;
+            this.btnPortState.Click += new System.EventHandler(this.btnPortState_Click);
+            // 
+            // btnHello
+            // 
+            this.btnHello.Location = new System.Drawing.Point(12, 85);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(75, 23);
+            this.btnHello.TabIndex = 14;
+            this.btnHello.Text = "Send";
+            this.btnHello.UseVisualStyleBackColor = true;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // rtbOutgoing
+            // 
+            this.rtbOutgoing.Location = new System.Drawing.Point(12, 267);
+            this.rtbOutgoing.Name = "rtbOutgoing";
+            this.rtbOutgoing.Size = new System.Drawing.Size(350, 31);
+            this.rtbOutgoing.TabIndex = 15;
+            this.rtbOutgoing.Text = "";
+            this.rtbOutgoing.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.rtbOutgoing_KeyPress);
+            // 
+            // btnHyperTerm
+            // 
+            this.btnHyperTerm.Location = new System.Drawing.Point(287, 237);
+            this.btnHyperTerm.Name = "btnHyperTerm";
+            this.btnHyperTerm.Size = new System.Drawing.Size(75, 23);
+            this.btnHyperTerm.TabIndex = 16;
+            this.btnHyperTerm.Text = "HyperTerm";
+            this.btnHyperTerm.UseVisualStyleBackColor = true;
+            this.btnHyperTerm.Click += new System.EventHandler(this.btnHyperTerm_Click);
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(12, 240);
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.Size = new System.Drawing.Size(257, 20);
+            this.txtCommand.TabIndex = 17;
+            this.txtCommand.Text = "5600000000002000040000000000010";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Ports";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "BaudRate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "DataBits";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "StopBits";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(259, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Parity";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(259, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "HandShaking";
+            // 
+            // lblLogger
+            // 
+            this.lblLogger.AutoSize = true;
+            this.lblLogger.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblLogger.Location = new System.Drawing.Point(20, 217);
+            this.lblLogger.Name = "lblLogger";
+            this.lblLogger.Size = new System.Drawing.Size(0, 13);
+            this.lblLogger.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 408);
+            this.Controls.Add(this.lblLogger);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.btnHyperTerm);
+            this.Controls.Add(this.rtbOutgoing);
+            this.Controls.Add(this.btnHello);
+            this.Controls.Add(this.btnPortState);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.lblRIStatus);
+            this.Controls.Add(this.lblDSRStatus);
+            this.Controls.Add(this.lblCTSStatus);
+            this.Controls.Add(this.lblBreakStatus);
+            this.Controls.Add(this.cboHandShaking);
+            this.Controls.Add(this.cboParity);
+            this.Controls.Add(this.cboStopBits);
+            this.Controls.Add(this.cboDataBits);
+            this.Controls.Add(this.cboBaudRate);
+            this.Controls.Add(this.cboPorts);
+            this.Controls.Add(this.rtbIncoming);
+            this.Controls.Add(this.btnGetSerialPorts);
+            this.Name = "Form1";
+            this.Text = "SerialComms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetSerialPorts;
+        private System.Windows.Forms.RichTextBox rtbIncoming;
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.ComboBox cboDataBits;
+        private System.Windows.Forms.ComboBox cboStopBits;
+        private System.Windows.Forms.ComboBox cboParity;
+        private System.Windows.Forms.ComboBox cboHandShaking;
+        private System.Windows.Forms.Label lblBreakStatus;
+        private System.Windows.Forms.Label lblCTSStatus;
+        private System.Windows.Forms.Label lblDSRStatus;
+        private System.Windows.Forms.Label lblRIStatus;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnPortState;
+        private System.Windows.Forms.Button btnHello;
+        private System.Windows.Forms.RichTextBox rtbOutgoing;
+        private System.Windows.Forms.Button btnHyperTerm;
+        private System.Windows.Forms.TextBox txtCommand;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label lblLogger;
+    }
+}
+
